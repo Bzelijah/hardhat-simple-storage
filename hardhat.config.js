325503a70b23b2a6446826e84dd3c66281838eb0
bzelijah <ilya.andreev@onlinesup.com>
2022-11-03 21:31:45 +0300

    add coverage and 100% test coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL ?? "https://eth-rinkbey";
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? "key";
+const CMC_API_KEY = process.env.CMC_API_KEY ?? "key";
 
 module.exports = {
   solidity: "0.8.8",
@@ -25,5 +28,13 @@ module.exports = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: CMC_API_KEY,
+    token: "MATIC"
   }
 };
--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,5 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect, assert } = require("chai");
+const {json} = require("hardhat/internal/core/params/argumentTypes");
 
 describe("SimpleStorage", () => {
   let simpleStorageFactory, simpleStorage;
@@ -23,4 +24,16 @@ describe("SimpleStorage", () => {
     const currentValue = await simpleStorage.retrieve();
     assert.equal(currentValue.toString(), expectedValue);
   });
+
+  it("Should add person with passed parameters to People array", async () => {
+    const expectedName = "Ilya";
+    const expectedNumber = "13";
+    const expectedResult = '[{"type":"BigNumber","hex":"0x0d"},"Ilya"]';
+
+    const transactionResponse = await simpleStorage.addPerson(expectedName, expectedNumber);
+    await transactionResponse.wait(1);
+
+    const people = await simpleStorage.people(0);
+    assert.equal(JSON.stringify(people), expectedResult);
+  })
 });
